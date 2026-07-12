ab5e9e404c71f01b043a030e466d5309f297e96a
Sayyam Shahid <ms2546983@gmail.com>
2026-07-12 19:44:12 +0500

    Hangman_Game

---
+++ Hangman_Game/main.py
@@ -0,0 +1,55 @@
+import random
+
+words = ["apple", "python", "house", "flower", "computer"]
+
+secret_word = random.choice(words)
+
+guessed_letters = []
+
+lives = 6 # 6 time user
+
+print("🎮 Welcome to Hangman!")
+print("Guess the hidden word!")
+
+# game loop , game will run 6 times
+while lives > 0:
+
+    # Display the word
+    display_word = ""
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+
+    print("\nWord:", display_word)
+    print("Lives Left:", lives)
+
+    # When user guessed whole letter
+    if "_" not in display_word:
+        print("\n🎉 Congratulations! You guessed the word:", secret_word)
+        break
+
+    # 
+    guess = input("Enter a letter: ").lower()
+
+    # Check if the user already guessed the letter
+    if guess in guessed_letters:
+        print("⚠ You already guessed that letter.")
+        continue
+
+    # Its save the letter into guessed
+    guessed_letters.append(guess)
+
+    # When user enter thr correct word
+    if guess in secret_word:
+        print("User Guess Correct ✅ ")
+    else:
+        print("User Guess Wrong ❌ ")
+        lives -= 1
+
+
+if lives == 0:
+    print("\n💀 Game Over!")
+    print("The word was:", secret_word)
